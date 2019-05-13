9a8e9fbb762822b2a9727947b66f6124e440d325
David <voihanskidavid@gmail.com>
2019-05-14 02:09:40 +0300

    created Test.cpp

---
+++ Test.cpp
@@ -0,0 +1,13 @@
+#include "badkan.hpp"
+#include<iostream>
+#include "range.hpp"
+#include "chain.hpp"
+#include "zip.hpp"
+#include "product.hpp"
+#include "powerset.hpp"
+#define COMMA ,
+using namespace itertools;
+
+int main(){
+    return 0;
+}
